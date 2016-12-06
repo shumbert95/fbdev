ce1cc68c508802366e99eec4f3f8ad674ec07ff5
Simon Humbert <shumbert@kernix.com>
2016-12-06 10:41:12 +0100

    Update navbar inte

---
+++ views/navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <a href="#">Accueil</a>
+                </li>
+                <li>
+                    <a href="#">Galerie</a>
+                </li>
+                <li>
+                    <a href="#">Règles</a>
+                </li>
+                <li>
+                    <a href="#">Connexion</a>
+                </li>
+            </ul>
+            <button type="button" id="participate" class="btn btn-secondary">Je participe</button>
+        </div>
+    </div>
+</nav>
