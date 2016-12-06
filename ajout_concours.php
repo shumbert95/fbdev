472fd2e2a895f91d25ea3c9c0871de4d9adfe4cc
Jérémy Moreau <jeremyfs.moreau@gmail.com>
2016-12-06 11:09:21 +0100

    création de la partie transfert de photo de la part d'un utilisateur, travail encore en cours

---
+++ ajout_concours.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title> Création d'un nouveau concours></title>
+		<link rel="stylesheet" type="text/css" href="http://jsy.fbdev.fr/public/css/ajout_concours.css">
+	</head>
+	<body>
+		<p> Créer un concours </p>
+		<table>
+			<tr>
+				<td>Test</td>
+				<td><input type="text" placeholder="test"> </td>
+			</tr>
+		</table>		
+	</body>
+</html>
